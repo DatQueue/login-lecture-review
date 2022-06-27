bd5329d91795104e669935b92c1aa129d50b5ce3
kikko <a01032762271@gmail.com>
2022-06-27 22:23:20 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,16 +7,15 @@ class UserStorage {
     name: ["Daegyu", "SiHyun", "Yurim"],
   };
 
-  static getUsers(...fields) {
+  static getUserInfo(id) {
     const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users);
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
     }, {});
-    console.log(newUsers);
-    return newUsers;
+    return userInfo;
   }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -14,22 +14,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      psWord = req.body.psWord;
-
-    const users = UserStorage.getUsers("id", "psWord");
-    const response = {};
-
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psWord[idx] === psWord) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "login failed!";
+    const user = new User(req.body);
+    const response = user.login();
+    console.log(response);
     return res.json(response);
   },
 };
