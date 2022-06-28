61eb24c884490581b4bbdbd71327e5a924e5cf12
kikko <a01032762271@gmail.com>
2022-06-28 21:11:44 +0900

    로그인 화면 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -16,7 +16,6 @@ const process = {
   login: (req, res) => {
     const user = new User(req.body);
     const response = user.login();
-    console.log(response);
     return res.json(response);
   },
 };
